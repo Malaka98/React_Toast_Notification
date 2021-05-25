150aa2f65a601a0f721f57664b1f2ded11780a80
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-26 01:04:45 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+class Notify {
+  static notifyTL() {
+    toast.info("🦄 Wow so easy!", {
+      position: "top-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  static notifyTC() {
+    toast.error("🦄 Wow so easy!", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  static notifyTR() {
+    toast.success("🦄 Wow so easy!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  static notifyBL() {
+    toast.warn("🦄 Wow so easy!", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  static notifyBC() {
+    toast.dark("🦄 Wow so easy!", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  static notifyBR() {
+    toast("🦄 Wow so easy!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container style={{margin: "auto", paddingTop: "200px"}}>
+        <Row>
+          <Col>
+            <Button
+              variant="primary"
+              size="lg"
+              style={{ margin: "10px" }}
+              onClick={Notify.notifyTL}
+            >
+              Top Left
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              size="lg"
+              style={{ margin: "10px" }}
+              onClick={Notify.notifyTC}
+            >
+              Top Center
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              size="lg"
+              style={{ margin: "10px" }}
+              onClick={Notify.notifyTR}
+            >
+              Top Right
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              variant="primary"
+              size="lg"
+              style={{ margin: "10px" }}
+              onClick={Notify.notifyBL}
+            >
+              Bottom Left
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              size="lg"
+              style={{ margin: "10px" }}
+              onClick={Notify.notifyBC}
+            >
+              Bottom Center
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              size="lg"
+              style={{ margin: "10px" }}
+              onClick={Notify.notifyBR}
+            >
+              Bottm Right
+            </Button>
+          </Col>
+        </Row>
+
+        <ToastContainer />
+      </Container>
     </div>
   );
 }
-
-export default App;
